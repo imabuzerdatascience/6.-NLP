687d77440dc1109205240aae00e37e72080e94fc
Mohammad Abuzer Khan <abuzerdatascience@gmail.com>
2026-07-25 08:42:03 +0545

    playing withspacy

--- Starting with NLP/NLP.py
+++ Starting with NLP/NLP.py
@@ -1,7 +1,7 @@
 # sentences tokeniztion 
 
 from nltk.tokenize import sent_tokenize 
-
+import spacy 
 sentences = '''
 Hello my name is abuzer khan , and i live in biratnagar six . I am datascientisst 
 and I work hard to fnd a job 
@@ -9,4 +9,28 @@ and I work hard to fnd a job
 ''' 
 
 sent = sent_tokenize(sentences) 
-print(sent)
+print(sent) 
+
+
+print("--------------------------------------------------------------------")
+
+# another one 
+text = '''
+I am learning NLP.
+I am learning Machine Learning!
+Are you learning AI?
+Yes, I am.
+
+''' 
+
+token = sent_tokenize(text) 
+
+#using for loop to print 
+for tokens in token : 
+    print(tokens)
+
+#direct print 
+print(token) 
+
+
+# playing with spacy  
