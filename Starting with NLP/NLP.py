fb6bc79eaaf9f06458577fe3bb43ab1b3d4c8ff0
Mohammad Abuzer Khan <abuzerdatascience@gmail.com>
2026-07-25 08:58:54 +0545

    revised some important concepts of nlp

--- Starting with NLP/NLP.py
+++ Starting with NLP/NLP.py
@@ -4,36 +4,54 @@ from nltk.tokenize import sent_tokenize
 import spacy  
 from nltk.tokenize import word_tokenize 
 
-sentences = '''
-Hello my name is abuzer khan , and i live in biratnagar six . I am datascientisst 
-and I work hard to fnd a job 
+# sentences = '''
+# Hello my name is abuzer khan , and i live in biratnagar six . I am datascientisst 
+# and I work hard to fnd a job 
 
-''' 
+# ''' 
 
-sent = sent_tokenize(sentences) 
-print(sent) 
+# sent = sent_tokenize(sentences) 
+# print(sent) 
 
 
-print("--------------------------------------------------------------------")
+# print("--------------------------------------------------------------------")
 
-# another one 
-text = '''
-I am learning NLP.
-I am learning Machine Learning!
-Are you learning AI?
-Yes, I am.
+# # another one 
+# text = '''
+# I am learning NLP.
+# I am learning Machine Learning!
+# Are you learning AI?
+# Yes, I am.
 
-''' 
+# ''' 
 
-token = sent_tokenize(text) 
+# token = sent_tokenize(text) 
 
-#using for loop to print 
-for tokens in token : 
-    print(tokens)
+# #using for loop to print 
+# for tokens in token : 
+#     print(tokens)
 
-#direct print 
-print(token) 
+# #direct print 
+# print(token) 
 
 
 
 # word tokenizer 
+# text = '''
+# Hello , She is my sister and she is came from Austrila.
+# ''' 
+# words_token = word_tokenize(text)  
+
+
+# print(words_token) 
+
+# character toeknization 
+text = "hello" 
+
+toekns = list(text)
+# print(toekns) 
+
+text2 = "artificial" 
+
+tokens2 = list(text2) 
+print(tokens2)
